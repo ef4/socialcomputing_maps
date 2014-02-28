7de869140f99a98b05087ccfc72aaa9bbf83aac2
Pranav Ramkrishnan <pranavr@mit.edu>
2014-02-28 10:16:40 -0500

    cm green

--- maps/views.py
+++ maps/views.py
@@ -41,6 +41,9 @@ def sanfrancisco_green(request):
 def cambridge_green(request):
 	return render_to_response('cambridge-green.html', {'URL_PREFIX':url_config.get_URL_PREFIX()})
 
+def cambridge_green2(request):
+	return render_to_response('cambridge-green2.html', {'URL_PREFIX':url_config.get_URL_PREFIX()})
+
 def cambridge_generations(request):
 	return render_to_response('cambridge-generations.html', {'URL_PREFIX':url_config.get_URL_PREFIX()})
 
